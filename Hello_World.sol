b78690f18c69062b25d9aee6cf068eb802759016
stewiezoolights <84042366+stewiezoolights@users.noreply.github.com>
2021-07-07 16:57:23 -0500

    Create Hello_World.sol

---
+++ Hello_World.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+
+pragma solidity 0.8.1;
+
+contract integerExample {
+    uint public myUint;
+    
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+}
