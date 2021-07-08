3fcbc37a339233f6a8f2a17228859dc8f87eb719
stewiezoolights <84042366+stewiezoolights@users.noreply.github.com>
2021-07-07 19:14:15 -0500

    Create StringExample.sol

---
+++ StringExample.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.1;
+
+contract StringExample{
+    string public myString = 'hello world!';
+    
+    function setMyString(string memory _myString) public {
+        myString = _myString;
+    }
+    
+    
+}
